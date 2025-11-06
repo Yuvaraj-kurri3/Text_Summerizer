88a9dfb1156784e5ee05cbbfd82dd89625d95a72
Yuvarajkurri <kurriyuvaraj13@gmail.com>
2025-11-06 13:08:30 +0530

    redis error fixed

--- backend/redisClient.js
+++ backend/redisClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 
-const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6379";
+const redisUrl = process.env.redis_url || "redis://127.0.0.1:6379";
 
 // Detect if Upstash (production)
 const isUpstash = redisUrl.startsWith("rediss://");
