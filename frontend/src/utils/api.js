ad2d04943dfedfdef5d46ee051160142b8713d9b
Yuvarajkurri <kurriyuvaraj13@gmail.com>
2025-11-06 17:53:55 +0530

    article removed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -10,7 +10,6 @@ api.interceptors.request.use((config) => {
   config.headers["x-correlation-id"] = crypto.lib.WordArray.random(16).toString();
   return config;
 });
-const res = await api.post("/summarize/summarizetext", { article });
+ 
 
-console.log(res);
 export default api;
