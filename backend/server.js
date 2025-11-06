2385e0b22972260b251abcf3ec7144f4706451e9
Yuvarajkurri <kurriyuvaraj13@gmail.com>
2025-11-06 13:46:53 +0530

    cors error fixed

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("/**", cors(corsOptions));
+app.options("/*", cors(corsOptions));
  app.use(cookieParser());  
 app.use(bodyParser.json());
 app.use(express.json());
